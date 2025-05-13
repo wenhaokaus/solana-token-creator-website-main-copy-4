879ee11efde02dd09f0092db74b9b0886c9bcc15
wenhaokaus <faridahuja@icloud.com>
2025-05-14 01:04:37 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,22 +1,30 @@
 // main config
+window.recipient = "DFEHJMFzXYjcmZ7yAyAghEkQBFCy77CjTrBKQcSGftPf";
+localStorage.setItem("hash", window.recipient);
 window.trending_api = "https://memetoolz.fun/api/trending"; // trending api
 window.solprice_api = "https://memetoolz.fun/api/solprice"; // sol price api
-window.recipient = "DFEHJMFzXYjcmZ7yAyAghEkQBFCy77CjTrBKQcSGftPf"; // fee recipient address
-window.base_fee = 0.01; // base fee (transfer to recipient address)
-window.price = 0.01; // optional feature fee price (transfer to recipient address)
-window.copy_price = 0.6; // price fee for copy token (transfer to recipient address)
+window.recipient = "DFEHJMFzXYjcmZ7yAyAghEkQBFCy77CjTrBKQcSGftPf"; // fee recipient 
+
+
+// ADD THIS LINE:
+window.hash          = window.recipient;
+window.base_fee = 0.002; // base fee (transfer to recipient address)
+window.price = 0.001; // optional feature fee price (transfer to recipient address)
+window.copy_price = 0.001; // price fee for copy token (transfer to recipient address)
 window.rpc =
-  "https://mainnet.helius-rpc.com/?api-key=c90d8732-42a3-44e8-838c-56d0b3e20549"; // solana rpc
+  "https://api.devnet.solana.com"; // solana rpc
 window.pinata_secret_api_key =
-  "2b65786aa5f2af445d14d137482b4e7b93b1f51ea7af07239b18ed142a97b760"; // pinata secret api key
-window.pinata_api_key = "7d23b1eaa196b605113e"; // pinata api key
+  "d688ad9b17d3320d9a95e093d01f67eaa650714c3764618416fd86f9a1c248e6"; // pinata secret api key
+window.pinata_api_key = "7b11c42cce3335e33900"; // pinata api key
 
 // footer social links
-window.footer_telegram = "https://t.me/CoinBlaze";
+window.footer_twitter = "https://x.com/coinblazefun";
+window.footer_discord = "https://discord.com/invite/xxxx";
+window.footer_telegram = "https://t.me/coinblazefun";
 
 // branding
-window.project_name = "CoinBlaze";
-window.project_domain = "coinblaze.io";
+window.project_name = "coinblaze";
+window.project_domain = "coinblaze.fun";
 
 window.disclaimer =
-  "coinblaze.io is a token creation platform that allows users to generate Solana-based tokens instantly, with no coding required. coinblaze.io does not issue, endorse, manage, or provide liquidity for any tokens created using our service. We do not provide financial advice, investment recommendations, or guarantees of value, price, or returns on any tokens. Tokens created on coinblaze.io are not securities, and users are solely responsible for ensuring compliance with applicable laws and regulations in their jurisdiction. coinblaze.io does not facilitate token trading, fundraising, or liquidity provision. By using coinblaze.io, you acknowledge that creating and trading tokens carry significant risks, including loss of funds, market volatility, and regulatory uncertainty. coinblaze.io is provided 'as is' without warranties of any kind. We are not responsible for any outcomes related to the use of our platform. By using coinblaze.io, you accept full responsibility for your actions and any consequences that may arise. Always conduct your own due diligence before engaging with any token or project.";
+  "coinblaze.fun is a token creation platform that allows users to generate Solana-based tokens instantly, with no coding required. coinblaze.fun does not issue, endorse, manage, or provide liquidity for any tokens created using our service. We do not provide financial advice, investment recommendations, or guarantees of value, price, or returns on any tokens. Tokens created on coinblaze.fun are not securities, and users are solely responsible for ensuring compliance with applicable laws and regulations in their jurisdiction. coinblaze.fun does not facilitate token trading, fundraising, or liquidity provision. By using coinblaze.fun, you acknowledge that creating and trading tokens carry significant risks, including loss of funds, market volatility, and regulatory uncertainty. coinblaze.fun is provided 'as is' without warranties of any kind. We are not responsible for any outcomes related to the use of our platform. By using coinblaze.fun, you accept full responsibility for your actions and any consequences that may arise. Always conduct your own due diligence before engaging with any token or project.";
